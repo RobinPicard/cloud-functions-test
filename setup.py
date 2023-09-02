0c2263b5e7774dc3f57537bdf4b66d24a12239b4
Robin Picard <83579270+RobinPicard@users.noreply.github.com>
2023-09-03 00:46:12 +0200

    End fix non-inclusion test_classes dir in the package in pypi (#6)

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup
+from setuptools import find_packages
 
 
 name = "cloud-function-test"
-version = "0.0.1"
+version = "0.0.5"
 
 setup(
     name=name,
     version=version,
     packages=["cloud_function_test"],
+    package_data={
+        'cloud_function_test': ['test_classes/*'],
+    },
+    license='apache-2.0',
     description="Test locally GCP Cloud Functions",
     url=f"https://github.com/RobinPicard/{name}",
+    download_url=f"https://github.com/RobinPicard/{name}/releases/download/v0.0.1/{name}-{version}.tar.gz",
     author='Robin Picard',
-    author_email='robin.picard@sciencespo.fr'
+    author_email='robin.picard@sciencespo.fr',
     install_requires=[
         'functions-framework',
+        'python-dotenv',
         'requests',
         'termcolor',
     ],
